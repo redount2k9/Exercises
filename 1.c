e0c4a59fad4e071d4daa7be4aca785470d29585e
redount2k9 <axinie_razvan@yahoo.com>
2017-09-18 12:29:07 +0300

    Update and rename 1.cpp to 1.c

--- 1.c
+++ 1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /* You are given two 32-bit numbers, N and M, and two bit positions, i and j. Write a
 method to set all bits between i and j in N equal to M (e.g., M becomes a substring of
 N located at i and starting at j).
@@ -5,10 +7,10 @@ EXAMPLE:
 Input: N = 10000000000, M = 10101, i = 2, j = 6
 Output: N = 10001010100 */
 
-void setBits(int& N, int M, int i, int j) {
+void setBits(int* N, int M, int i, int j) {
 	M = M << i; // shift M i positions to the left 
 			   // since M is a substring we don't need to worry about j
-	N = N | M; // set the bits in N according to M 
+	*N = *N | M; // set the bits in N according to M 
 	return;
 }
 
